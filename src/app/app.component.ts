c2da8316555e97164cac98fcedb161d2440f27ba
Joey <hostingchick@gmail.com>
2017-10-21 13:58:03 -0500

    Created Input
    Created the input field and set a display for the field input.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+export class CharInfo {
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'The Jedi Archives';
+  people = 'Character';
+  character: CharInfo = { name: ''};
 }
