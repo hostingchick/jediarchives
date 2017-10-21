0a99a789adb5fe823c36e4307ae3362d7f46e8e3
Joey <hostingchick@gmail.com>
2017-10-21 15:43:49 -0500

    Added Classes
    Created classes and added options to web page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,100 @@
 import { Component } from '@angular/core';
 
-export class CharInfo {
+export class SearchInfo {
+  name: string;
+  option: string;
+}
+
+export class Character {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string;
+  species: string;
+  vehicles: string;
+  starships: string;
+  edited: string;
+  created: string;
+  url: string;
+}
+
+export class Planet {
   name: string;
 }
+
+export class Species {
+  name: string;
+}
+
+export class Starship {
+  name: string;
+}
+
+export class Vehicle {
+  name: string;
+}
+
+const SEARCHLIST: SearchInfo[] = [
+  { name: 'Characters', option: 'people' },
+  { name: 'Species', option: 'species' },
+  { name: 'Planets', option: 'planets' },
+  { name: 'Starships', option: 'starships' },
+  { name: 'Vehicles', option: 'vehicles' }
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'The Jedi Archives';
   people = 'Character';
-  character: CharInfo = { name: ''};
+  searchUrl = '';
+  searhoption: SearchInfo = {
+    name: '',
+    option: ''
+  };
+  searches = SEARCHLIST;
+  character: Character = {
+    name: '',
+    height: '',
+    mass: '',
+    hair_color: '',
+    skin_color: '',
+    eye_color: '',
+    birth_year: '',
+    gender: '',
+    homeworld: '',
+    films: '',
+    species: '',
+    vehicles: '',
+    starships: '',
+    edited: '',
+    created: '',
+    url: ''
+  };
+  planet: Planet = {
+    name: ''
+  };
+  species: Species = {
+    name: ''
+  };
+  starship: Starship = {
+    name: ''
+  };
+  vehicle: Vehicle = {
+    name: ''
+  };
+
+  onSelect(search): void {
+    this.searchUrl = 'https://swapai.co/api/' + search.option;
+  }
 }
