82f2fbfd329ee6f7057da21a2ee80ed6b85db7ad
Grow <jgrow@olin.wustl.edu>
2017-10-24 11:20:42 -0500

    form validation added.  display note for empty set.  add reset form.

--- src/app/search.component.ts
+++ src/app/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Http, Headers } from '@angular/http';
 import { Form } from '@angular/forms'
@@ -40,6 +40,7 @@ export class SearchComponent {
     option: ''
   };
   searches = SEARCHLIST;
+  noResults = '';
   characters: Characters[];
   charResult: Characters;
   constructor(
@@ -50,31 +51,20 @@ export class SearchComponent {
   searchData(search, question) {
       //Create the api url
       this.searchUrl = 'https://swapi.co/api'+ search.replace('/search','') + '?search=' + question.toUpperCase();
-
       // Make the HTTP request:
       return this.http.get(this.searchUrl)
       .map(res => res.json())
       .subscribe(
         data => {
-          this.results = data.results;
-          console.log(this.results);
-          return JSON.stringify(this.results);
-         },
+          if(data) {
+            this.results = data.results;
+            this.noResults = (data.length > 0) ? '' : 'Nothing to see here!';
+            console.log(this.noResults);
+            console.log(this.results);
+            return JSON.stringify(this.results);
+          }
+      },
         err => { console.log('Something went awry!'); }
       );
     }
-
-    searchSubData(url) {
-        // Make the HTTP request:
-        return this.http.get(url)
-        .map(res => res.json())
-        .subscribe(
-          data => {
-            this.subResults = data.subResults;
-            console.log(this.subResults);
-            return JSON.stringify(this.subResults);
-           },
-          err => { console.log('Something went awry!'); }
-        );
-      }
   }
